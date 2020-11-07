32bce3933e6fb16025dd8a21d5fac67c4d8d6feb
Katie Sloan <katie.a.sloan@gmail.com>
2020-11-07 12:09:59 +0000

    Populated models folder

---
+++ models/product.py
@@ -0,0 +1,9 @@
+class Product:
+
+    def __init__(self, name, description, stock_quantity, buying_cost, selling_price, id = None):
+        self.name = name
+        self.description = description
+        self.stock_quantity = stock_quantity
+        self.buying_cost = buying_cost
+        self.selling_price = selling_price
+        self.id = id
