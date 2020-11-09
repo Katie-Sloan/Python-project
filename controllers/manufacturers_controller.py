a45ee3d552cfbb97c4a97daaf9eb883146c4263c
Katie Sloan <katie.a.sloan@gmail.com>
2020-11-09 10:58:45 +0000

    Modified controllers, repositories and templates

--- controllers/manufacturers_controller.py
+++ controllers/manufacturers_controller.py
@@ -1,48 +1,48 @@
-from flask import Flask, render_template, request, redirect
-from flask import Blueprint
+from flask import Flask, Blueprint, render_template, request, redirect
+from repositories import manufacturer_repository, product_repository
 from models.manufacturer import Manufacturer
-import repositories.manufacturer_repository as manufacturer_repository
+from models.product import Product
 
 manufacturers_blueprint = Blueprint("manufacturers", __name__)
 
 @manufacturers_blueprint.route("/manufacturers", methods=["GET"])
 def manufacturers():
     manufacturers = manufacturer_repository.select_all()
-    return render_template("manufacturers/index.html", manufacturers = manufacturers)
+    return render_template("manufacturers/index.html", manufacturers=manufacturers)
 
-@manufacturers_blueprint.route("/manufacturers/<id>", methods=["GET"])
-def show(id):
-    manufacturer = manufacturer_repository.select(id)
-    return render_template("manufacturers/show.html", manufacturer=manufacturer)
+# @manufacturers_blueprint.route("/manufacturers/<id>", methods=["GET"])
+# def show(id):
+#     manufacturer = manufacturer_repository.select(id)
+#     return render_template("manufacturers/show.html", manufacturer=manufacturer)
 
-@manufacturers_blueprint.route("/manufacturers/add", methods=["GET"])
-def add_manufacturer():
-    products = product_repository.select_all()
-    return render_template("products/index.html", products = products)
+# @manufacturers_blueprint.route("/manufacturers/add", methods=["GET"])
+# def add_manufacturer():
+#     products = product_repository.select_all()
+#     return render_template("products/index.html", products = products)
 
-@manufacturers_blueprint.route("/manufacturers", methods=["POST"])
-def create_manufacturer():
-    name = request.form["name"]
+# @manufacturers_blueprint.route("/manufacturers", methods=["POST"])
+# def create_manufacturer():
+#     name = request.form["name"]
 
-    manufacturer = Manufacturer(name)
-    manufacturer_repository.save(manufacturer)
-    return redirect(/manufacturers)
+#     manufacturer = Manufacturer(name)
+#     manufacturer_repository.save(manufacturer)
+#     return redirect(/manufacturers)
 
-@manufacturers_blueprint.route("/manufacturers/<id>/edit", methods=["GET"])
-def edit_manufacturer(id):
-    manufacturer = manufacturer_repository.select(id)
-    return render_template("manufacturers.edit.html",  manufacturer = manufacturer)
+# @manufacturers_blueprint.route("/manufacturers/<id>/edit", methods=["GET"])
+# def edit_manufacturer(id):
+#     manufacturer = manufacturer_repository.select(id)
+#     return render_template("manufacturers.edit.html",  manufacturer = manufacturer)
 
-@manufacturers_blueprint.route("/manufacturers/<id>", methods=["POST"])
-def update_manufacturer():
-    name = request.form["name"]
+# @manufacturers_blueprint.route("/manufacturers/<id>", methods=["POST"])
+# def update_manufacturer():
+#     name = request.form["name"]
 
-    manufacturer = Manufacturer(name)
-    manufacturer_repository.save(manufacturer)
-    return redirect(/manufacturers)
+#     manufacturer = Manufacturer(name)
+#     manufacturer_repository.save(manufacturer)
+#     return redirect(/manufacturers)
 
-@manufacturers_blueprint.route("/manufacturers/<id>/delete", methods=["POST"])
-def delete_manufacturer(id):
-    manufacturer_repository.delete(id)
-    return redirect('/manufacturers')
+# @manufacturers_blueprint.route("/manufacturers/<id>/delete", methods=["POST"])
+# def delete_manufacturer(id):
+#     manufacturer_repository.delete(id)
+#     return redirect('/manufacturers')
 
--- repositories/manufacturer_repository.py
+++ repositories/manufacturer_repository.py
@@ -2,6 +2,7 @@ from db.run_sql import run_sql
 
 from models.manufacturer import Manufacturer
 from models.product import Product
+import repositories.product_repository as product_repository
 
 def save(manufacturer):
     sql = "INSERT into manufacturers (name) VALUES (%s) RETURNING *"
@@ -18,7 +19,7 @@ def select_all():
     results = run_sql(sql)
 
     for row in results:
-        manufacturer = Manufacturer(row['name'], row['id'])
+        manufacturer = Manufacturer(row['name'])
         manufacturers.append(manufacturer)
     return manufacturers
 
@@ -43,18 +44,18 @@ def delete(id):
 
 def update(manufacturer):
     sql = "UPDATE manufacturers SET (name) = (%s) WHERE id = %s"
-    values = [manufacturer.name]
+    values = [manufacturer.name, manufacturer.id]
     run_sql(sql, values)
 
-def products(manufacturer):
-    products = []
+# def products(manufacturer):
+#     products = []
 
-    sql = "SELECT * FROM products WHERE manufacturer_id = %s"
-    values = [manufacturer.id]
-    results = run_sql(sql, values)
+#     sql = "SELECT * FROM products WHERE manufacturer_id = %s"
+#     values = [manufacturer.id]
+#     results = run_sql(sql, values)
 
-    for row in results:
-        product = Product(row['name'], row['description'], row['stock_quantity'], row['buying_cost'], row['selling_price'], manufacturer)
-        products.append(product)
-    return products
+#     for row in results:
+#         product = Product(row['name'], row['description'], row['stock_quantity'], row['buying_cost'], row['selling_price'], manufacturer)
+#         products.append(product)
+#     return products
 
